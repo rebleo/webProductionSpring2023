7de8ee875c21203ff30b299ac9461df9cfaa00e1
rebleo <rml444@nyu.edu>
2023-04-03 14:13:32 -0400

    style object code

--- week09/code/theMessage.js
+++ week09/code/theMessage.js
@@ -21,3 +21,32 @@ function potato(){
 let thePrice = 5;
 let theQuantity = 50;
 let theTotal = thePrice * theQuantity;
+
+console.log("js!");
+
+
+
+// let theButton = document.querySelector("button");
+// let theTxt = document.querySelector('h2');
+// let theBody = document.querySelector('body');
+// console.log(theTxt)
+// //
+// theButton.addEventListener('click', theClick);
+
+// function theClick(){
+//  console.log("click!!!");
+//  theButton.style.backgroundColor = "yellow";
+//  theButton.style.fontSize = 80;
+//  theButton.style.color = "blue";
+//  theBody.style.backgroundColor =  "purple";
+ 
+// }
+
+
+// document.addEventListener('keydown', myFunction)
+
+// function myFunction() {
+//  console.log("yay!");
+//  theTxt.textContent = "you pressed any key!!"
+// }
+
