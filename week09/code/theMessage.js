b45f5b1ba7aebafe9477be9dcd18d34ba583259a
rebleo <rml444@nyu.edu>
2023-04-03 13:36:05 -0400

    js lecture notes

---
+++ week09/code/theMessage.js
@@ -0,0 +1,11 @@
+console.log("hello javascript!")
+
+let theName = "rebecca";
+let theNumber = 30;
+
+console.log(theName + " " + theNumber);
+//variable
+const theButton = document.querySelector("button");
+console.log(typeof theButton)
+
+
