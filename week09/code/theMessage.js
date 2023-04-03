8c0224add680c604b9bb2e84a888d45d24a4fbff
rebleo <rml444@nyu.edu>
2023-04-03 13:55:05 -0400

    init js

--- week09/code/theMessage.js
+++ week09/code/theMessage.js
@@ -1,11 +1,23 @@
 console.log("hello javascript!")
-
+//variables
 let theName = "rebecca";
 let theNumber = 30;
-
 console.log(theName + " " + theNumber);
-//variable
+// grabbing html elements
+// 
 const theButton = document.querySelector("button");
-console.log(typeof theButton)
+console.log(theButton)
+let thePara = document.querySelector("p")
+// adding an event listener to said element
+theButton.addEventListener("click", potato)
+//writing yr own function that does stuff
+function potato(){
+    console.log("yay - a click!");
+    thePara.style.backgroundColor = "pink"
+
+}
 
 
+let thePrice = 5;
+let theQuantity = 50;
+let theTotal = thePrice * theQuantity;
